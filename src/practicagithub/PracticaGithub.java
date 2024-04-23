80834a68c2ab5e560838e92f5d1b666db56ef5a3
JaviQuilumba <javier0611quilumba@gmail.com>
2024-04-22 22:15:12 -0500

    Modificado Javier

--- src/practicagithub/PracticaGithub.java
+++ src/practicagithub/PracticaGithub.java
@@ -7,7 +7,7 @@ public class PracticaGithub {
     
     public static void main(String[] args) {
         System.out.println("Hola, Mundo!");
-System.out.println("Prueba modificar git compañero Javierr");
+System.out.println("Prueba modificar git compañero lunes");
     }
     
 }
