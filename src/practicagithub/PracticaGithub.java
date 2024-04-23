7f31828f9c7b6da19d40787d2229b9298c3d0d59
JaviQuilumba <javier0611quilumba@gmail.com>
2024-04-22 21:48:41 -0500

    Modificado javier principal

--- src/practicagithub/PracticaGithub.java
+++ src/practicagithub/PracticaGithub.java
@@ -7,6 +7,7 @@ public class PracticaGithub {
     
     public static void main(String[] args) {
         System.out.println("Hola, Mundo!");
+System.out.println("Prueba modificar git compañero");
     }
     
 }
